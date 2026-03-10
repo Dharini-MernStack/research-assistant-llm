18ac12b27a95832ca88947df7e943b289ca066d5
Dharini-MernStack <dharinisrinivasan.94@gmail.com>
2026-03-10 16:35:30 +0530

    fix: add lib/api.js SDK file

---
+++ frontend/lib/api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+const client = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+const researchAssistant = {
+    uploadPDF: async (file, question = "Summarize this document in 3 sentences.") => {
+        const formData = new FormData();
+        formData.append("pdf", file);
+        formData.append("question", question);
+
+        const res = await client.post("/upload", formData, {
+            headers: { "Content-Type": "multipart/form-data" },
+        });
+
+        return res.data;
+    },
+
+    askQuestion: async (filename, question) => {
+        const res = await client.post("/upload/ask", { filename, question });
+        return res.data;
+    },
+
+    ping: async () => {
+        const res = await client.get("/");
+        return res.data;
+    },
+};
+
+export default researchAssistant;
